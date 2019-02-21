83cc91e225c1a068297691f0bfae21b23a5ef762
DvtNmk <devotion.nk@gmail.com>
2019-02-21 16:09:52 +0700

    [Initial project]

---
+++ Thread-State/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Thread_State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Creating and initializing threads 
+            Thread TR1 = new Thread(new ThreadStart(job));
+            Thread TR2 = new Thread(new ThreadStart(job));
+
+            Console.WriteLine("ThreadState of TR1 thread" +
+                             " is: {0}", TR1.ThreadState);
+
+            Console.WriteLine("ThreadState of TR2 thread" +
+                             " is: {0}", TR2.ThreadState);
+
+            // Running state  TR1
+            TR1.Start();
+
+            Console.WriteLine("ThreadState of TR1 thread " +
+                               "is: {0}", TR1.ThreadState);
+            // Stopped || Not Runnable state
+            Thread.Sleep(4000);
+            Console.WriteLine("ThreadState of TR1 thread " +
+                   "is: {0}", TR1.ThreadState);
+
+            //Aborted
+            TR1.Abort();
+            Console.WriteLine("ThreadState of TR1 thread " +
+                   "is: {0}", TR1.ThreadState);
+
+            //Running state TR2
+            TR2.Start();
+            Console.WriteLine("ThreadState of TR2 thread" +
+                             " is: {0}", TR2.ThreadState);
+
+            Console.WriteLine("Threads completed");
+            Console.ReadKey();
+        }
+
+        // Static method 
+        public static void job()
+        {
+            Thread.Sleep(2000);
+        }
+    }
+}
--- Thread-ex1/Program.cs
+++ Thread-ex1/Program.cs
@@ -2,14 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Thread_ex1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static Thread _thread;
+        public static void Main(string[] args)
+        { //Creating thread object to strat it
+            _thread = new Thread(new ThreadStart(ThreadB));
+            _thread.Name = "threadB";
+            
+            _thread = new Thread(new ThreadStart(ThreadA));
+            _thread.Name = "threadA";
+
+            Console.WriteLine("Threads started :");
+            // Start thread B
+            _thread.Start();
+            //Thread A executes 10 times
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Thread : A");
+            //}
+
+            Console.WriteLine("Threads completed");
+            Console.ReadKey();
+        }
+        public static void ThreadA()
         {
+            //Executes thread A 10 times
+            for (int i = 0; i < 10; i++)
+            {
+
+                Console.WriteLine(_thread.Name);
+                
+            }
+        }
+
+        public static void ThreadB()
+        {
+            //Executes thread B 10 times
+            for (int i = 0; i < 10; i++)
+            {
+
+                Console.WriteLine(_thread.Name);
+                //if(i>=5)
+                //{
+                //    _thread.Abort();
+
+                //}
+            }
         }
     }
 }
